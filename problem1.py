774d4b3c7bd9fd65690a79bb77fd9f5f4a1bb58b
AllanCheboiwo <kiplongeiallan@gmail.com>
2024-12-26 17:48:53 -0800

    problem1

---
+++ problem1.py
@@ -0,0 +1,16 @@
+sol=[]
+def problem1(m,n,lim):
+    if(n>=lim):
+        return 0
+    else:
+        sol.append(n)
+        problem1(m,n+m,lim)
+def main():
+    problem1(5,5,1000)
+    problem1(3,3,1000)
+    final = set(sol)
+
+    print(sum(final))
+
+
+main()
